982a2a64cb6aaf8281a598ba981fb9b4437bbdd9
Fayik <veznedaroglufayik2@gmail.com>
2022-07-31 23:25:35 +0300

    C_data_structer

---
+++ Data_Structure_Work.c
@@ -0,0 +1,33 @@
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int dizi[10]={1,2,3,4,5,6,7,8,9,10};
+
+    int result=(sizeof(dizi)/sizeof(dizi[0]));
+
+    // printf("%d",&dizi[1]);
+
+    for (int i = 0; i <= result; i++)
+    {
+        printf("%d \n",&dizi[i]);
+    }
+    //ARD arda gelmiyor ancak ardışık olarak 4er 4er bellekte yer atlayıp adrest tutmaktadırlar.
+    //Bunun sebebi ise int dizimin değişken tipli olması ve int tipinin bellkete 4 byte yer kaplamasından kaynaklanmaktadır.
+    //Tabi döngü kullandığım için bellketeki adresler 
+    //Yada evet ardışık olarak sıralanmışlar diyebiliriz.Çünkü dizi[] arrayi ile en başta 10 elemanlı bellkete kendimize bir yer tahsis etmiş oluruz.öö
+
+    
+
+
+
+
+
+	// printf("%d",dizi[0],"\n");
+	printf("%d",result);
+
+	
+	return 0;
+}
